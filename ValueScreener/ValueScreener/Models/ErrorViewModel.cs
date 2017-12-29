16270e0ac0f7a8f1404a61976a3c47fa06abf401
HUSSER Julien <julien.husser@leonteq.com>
2017-12-29 14:46:33 +0100

    Initial Scaffolding commit

---
+++ ValueScreener/ValueScreener/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ValueScreener.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
