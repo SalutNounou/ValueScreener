209b745dfb8af2913bb22847661faf428c55f770
HUSSER Julien <julien.husser@leonteq.com>
2017-12-29 15:55:51 +0100

    Stock Repository

---
+++ ValueScreener/ValueScreener/Controllers/StocksController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ValueScreener.Data;
+using ValueScreener.Models.Domain;
+
+namespace ValueScreener.Controllers
+{
+    public class StocksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StocksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stocks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Stocks.ToListAsync());
+        }
+
+        // GET: Stocks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stock = await _context.Stocks
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return View(stock);
+        }
+
+        // GET: Stocks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stocks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Ticker,Name,Sector,Industry,Country,Currency")] Stock stock)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(stock);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(stock);
+        }
+
+        // GET: Stocks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stock = await _context.Stocks.SingleOrDefaultAsync(m => m.Id == id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return View(stock);
+        }
+
+        // POST: Stocks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Ticker,Name,Sector,Industry,Country,Currency")] Stock stock)
+        {
+            if (id != stock.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(stock);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StockExists(stock.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(stock);
+        }
+
+        // GET: Stocks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stock = await _context.Stocks
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return View(stock);
+        }
+
+        // POST: Stocks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var stock = await _context.Stocks.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StockExists(int id)
+        {
+            return _context.Stocks.Any(e => e.Id == id);
+        }
+    }
+}
